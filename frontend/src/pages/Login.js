f7d0e3c2572780e1d701d94c9f637a1112feaa28
Your Name <you@example.com>
2026-01-07 17:57:19 +0530

    update app

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     const handleSubmit = (e)=>{
         e.preventDefault()
 
-        axios.post("http://localhost:8000/api/v1/login", {
+        axios.post(process.env.BACKEND_URL+"login", {
             email,
             password
         })
